212304560b5984988b6d885fc5520b88d9f92f19
BruceNirun19 <nirun.h@planetcomm.com>
2023-07-24 01:21:30 +0700

    192

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//admin routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])
     ->name('dashboard');
+
+//profile route
+Route::get('profile' , [ProfileController::class , 'index'])
+    ->name('profile');
+Route::post('profile/update' , [ProfileController::class , 'updateProfile'])
+    ->name('profile.update');
